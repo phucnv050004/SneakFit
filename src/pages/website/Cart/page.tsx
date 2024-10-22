e4cb2295eecff3ca39552efb2f7ba60efda32bf1
Ngo Tien Dat <ngotiendat69@gmail.com>
2024-10-22 19:49:41 +0700

    cart

---
+++ src/pages/website/Cart/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Cart from "./_components/Cart";
+import ProductViewed from "./_components/ProductViewed";
+
+
+const pageCart = () => {
+  return (
+    <div>
+      <Cart />
+      <ProductViewed />
+    </div>
+  );
+};
+
+export default pageCart;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import LayoutAdmin from "@/pages/admin/LayoutAdmin";
 import AdminAddProduct from "@/pages/admin/product/Add";
 import AdminProductList from "@/pages/admin/product/List";
+import pageCart from "@/pages/website/Cart/page";
 import pageHome from "@/pages/website/home/page";
 import layoutWebsite from "@/pages/website/layout";
 import pageDetail from "@/pages/website/ProductDetil/page";
@@ -14,6 +15,7 @@ const Router = () => {
       children: [
         { index: true, Component: pageHome },
         { path: "detil/:id", Component: pageDetail },
+        { path: "cart", Component: pageCart },
       ],
     },
 
